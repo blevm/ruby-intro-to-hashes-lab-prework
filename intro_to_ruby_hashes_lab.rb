7d9604d892886dbffe764dc50741354c970000f8
Michelle Blevins <mjblevins7@gmail.com>
2018-03-25 23:00:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,8 @@ def actor
 end
 
 def monopoly
-	monopoly_with_second_tier = {
-		:railroads=>{
-			:pieces => 4,
-			:names => {},
-			:rent_in_dollars => {}
-		}
+	monopoly = {
+		:railroads=>{}
 	}
 
 end
